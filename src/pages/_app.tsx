f9a178402b3acd869400c97791a9677f9f106a25
Nikita Koshelev <nkoshelev@icloud.com>
2023-03-17 14:50:17 +0300

    add gtm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Script from 'next/script'
 import Layout from '@/pages/layout'
 import { Inter } from 'next/font/google'
 import '@/styles/global.css'
@@ -24,6 +25,15 @@ export default function App({ Component, pageProps }: AppProps) {
           <link rel='icon' href='/favicons/favicon.svg' type='image/svg+xml' />
           <link rel='apple-touch-icon' href='/favicons/apple-touch-icon.png' />
         </Head>
+        <Script id='google-tag-manager' strategy='afterInteractive'>
+          {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WC98VR4');
+      `}
+        </Script>
         <Component {...pageProps} />
       </>
     </Layout>
